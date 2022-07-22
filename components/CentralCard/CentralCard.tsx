c4bb0bf510b5fe17b00df23468cce35ac96e92e1
gusfonseca13 <gustavo.fonseca13@hotmail.com>
2022-07-22 18:09:30 -0300

    Teste de resolucao de problema no vh em paginas web de celular

--- components/CentralCard/CentralCard.tsx
+++ components/CentralCard/CentralCard.tsx
@@ -1,14 +1,24 @@
+import { useEffect } from "react";
+
 import classes from "./CentralCard.module.css";
 
 import CentralCardHeader from "./Header/CentralCardHeader";
 import CentralCardBody from "./Body/CentralCardBody";
 
 const CentralCard = () => {
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  }, []);
+
   return (
-      <div className={classes.card}>
-        <CentralCardHeader />
-        <CentralCardBody />
-      </div>
+    <div className={classes.card}>
+      <CentralCardHeader />
+      <CentralCardBody />
+    </div>
   );
 };
 
