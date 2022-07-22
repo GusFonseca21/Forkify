d8603ef7694f9f1257c46a811c9a2abb28119820
gusfonseca13 <gustavo.fonseca13@hotmail.com>
2022-07-22 18:14:24 -0300

    Teste solucao de problema no vh de browser de celulares

--- components/CentralCard/CentralCard.tsx
+++ components/CentralCard/CentralCard.tsx
@@ -6,13 +6,6 @@ import CentralCardHeader from "./Header/CentralCardHeader";
 import CentralCardBody from "./Body/CentralCardBody";
 
 const CentralCard = () => {
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      // We execute the same script as before
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    });
-  }, []);
 
   return (
     <div className={classes.card}>
