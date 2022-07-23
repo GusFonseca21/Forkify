19f7783a11f63350b6f8e60cfaafd186cf790534
gusfonseca13 <gustavo.fonseca13@hotmail.com>
2022-07-23 18:20:47 -0300

    Testes em fetch e teste dos dados no layout

--- src/components/store/fetch-recipes-context.tsx
+++ src/components/store/fetch-recipes-context.tsx
@@ -1,20 +1,20 @@
 import React, {useState} from "react";
 
-const FetchRecipesContext = React.createContext<{
-  searchInput: string;
-  foundRecipes: string[];
-}>({
-  searchInput: "",
-  foundRecipes: [],
-});
+// const FetchRecipesContext = React.createContext<{
+//   searchInput: string;
+//   foundRecipes: string[];
+// }>({
+//   searchInput: "",
+//   foundRecipes: [],
+// });
 
-const FetchRecipesContextProvider: React.FC<{children: React.ReactNode}> = (props) => {
-    const [inputText, setInputText] = useState("");
+// const FetchRecipesContextProvider: React.FC<{children: React.ReactNode}> = (props) => {
+//     const [inputText, setInputText] = useState("");
 
     
 
 
-    return <FetchRecipesContext.Provider>
-        {props.children}
-    </FetchRecipesContext.Provider>
-};
+//     return <FetchRecipesContext.Provider>
+//         {props.children}
+//     </FetchRecipesContext.Provider>
+// };
