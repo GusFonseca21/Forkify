5e77ac07bcdca8d4d155a8c25c953e2557dad8eb
gusfonseca13 <gustavo.fonseca13@hotmail.com>
2022-07-31 00:46:53 -0300

    Todos os testes para resolver o problema com o addressbar e o tamanho da pagina falharam

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -7,9 +7,6 @@ import classes from "./Background.module.css";
 import AddRecipeModal from "./AddRecipeModal/AddRecipeModal";
 
 const Background: React.FC<{ children: React.ReactNode }> = (props) => {
-  const stylesCtx = useContext(StylesContext)
-
-  const teste: boolean = false;
   return (
     <>
       <AddRecipeModal />
