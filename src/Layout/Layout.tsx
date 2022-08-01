2e97e598de7979ca79c837f6293d1090ff811ade
gusfonseca13 <gustavo.fonseca13@hotmail.com>
2022-08-01 20:31:30 -0300

    Funcionalidade de Servings finalizada

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -3,7 +3,6 @@ import Background from "../components/Background/Background";
 import Body from "../components/CentralCard/Body/Body";
 import FoundRecipes from "../components/CentralCard/Body/FoundRecipes/FoundRecipes";
 import SelectedRecipe from "../components/CentralCard/Body/SelectedRecipe/SelectedRecipe";
-import SelectedRecipeComponents from "../components/CentralCard/Body/SelectedRecipe/SelectedRecipeComponent/SelectedRecipeComponents";
 import CentralCard from "../components/CentralCard/CentralCard";
 import Footer from "../components/CentralCard/Footer/Footer";
 import Header from "../components/CentralCard/Header/Header";
