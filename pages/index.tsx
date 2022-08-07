3f615e00423bc2184ab358a8bb2d185fb170bd13
gusfonseca13 <gustavo.fonseca13@hotmail.com>
2022-08-06 22:12:48 -0300

    v1.04

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,10 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-       
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0,  user-scalable=0"
+        />
       </Head>
       <FoundRecipes />
       <SelectedRecipe />
